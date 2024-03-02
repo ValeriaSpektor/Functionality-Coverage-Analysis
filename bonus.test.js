4340a0f97975e75855b081491b5e773e1f1dd29d
Valeria Spektor <vlapin84@gmail.com>
2024-03-02 15:42:56 +0200

    added exampeles

--- bonus.test.js
+++ bonus.test.js
@@ -6,4 +6,16 @@ test('>=50', () => {
 
 test('<50', () => {
     expect(calculateBonus(20,30)).toEqual(50);
+});
+
+test('0', () => {
+    expect(calculateBonus(0,0)).toEqual(0);
+});
+
+test('>50', () => {
+    expect(calculateBonus(1,50)).toEqual(50);
+});
+
+test('negative number', () => {
+    expect(calculateBonus(-1,15)).toEqual(0);
 })
